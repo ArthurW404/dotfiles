d75d6af1593ee92a6ffcbda6174c6623b06e3142
Arthur Wang <ArthurWang2000@hotmail.com>
2020-09-05 16:19:13 +1000

    Weather block now changes unicode based on weather

---
+++ .config/i3blocks/blocks/i3weather.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+curr_weather=`curl -Ss 'https://wttr.in?0&T&Q' | cut -c 16- | head -2 | xargs echo`
+
+if echo $curr_weather | egrep '^(Sunny|Clear)' > /dev/null
+then
+    echo -n ☀️
+elif echo $curr_weather | egrep '^Partly cloudy' > /dev/null
+then
+    echo -n ⛅
+elif echo $curr_weather | egrep '^Cloudy' > /dev/null
+then
+    echo -n ☁️
+else 
+    echo -n "?"
+fi
+
+echo " $curr_weather"
